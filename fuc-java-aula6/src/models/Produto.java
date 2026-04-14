a72a76d86877f8d08ee972074eb34e926ba1d0d1
Ho Kyung Kim <kimhokyungkim@gmail.com>
2026-04-14 16:40:24 -0300

    ex1

---
+++ fuc-java-aula6/src/models/Produto.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Produto {
+
+	 	private String nome;
+	    private double valor;
+	    private int unidades;
+	    
+	    public String getNome() {
+	        return this.nome;
+	    }
+	    // Getter para double
+	    public double getValor() {
+	        return this.valor;
+	    }
+	    public int getUnidade() {
+	        return this.unidades;
+	    }
+	    public void setNome(String nomeProduto) {
+	        this.nome = nomeProduto;
+	    }
+	    public void setValor(double valorProduto) {
+	        if (valorProduto >= 0) this.valor = valorProduto;
+	    }
+	    
+	    public void setUnidades(int unidadesProduto) {
+	        if (unidadesProduto >= 0) this.valor = unidadesProduto;
+	    }
+	
+}
